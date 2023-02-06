3a1f56604324a1f4520efdad1b949bb6bc0a6c38
Ziza15 <zivkodaka9@gmail.com>
2023-02-06 21:17:24 +0100

    Final

--- src/components/menu-page/curentlyMenu/MealMenu.js
+++ src/components/menu-page/curentlyMenu/MealMenu.js
@@ -1,28 +1,29 @@
 import classes from "./MealMenu.module.css";
 import img from "../../../assets/btnImg.svg";
-import { useContext} from "react";
+import { useContext } from "react";
 import AddCart from "../../../store/add-cart";
 const MealMenu = (props) => {
   const ctx = useContext(AddCart);
-  let inCart=false
-  let amount = 1
-  
+  let inCart = false;
+  let amount = 1;
+
   const addItem = () => {
     if (ctx.cartItems.length === 0) {
       ctx.addItemsToCart({
         id: props.id,
         name: props.name,
         price: props.price,
-        amount: amount
+        amount: amount,
       });
     } else {
       ctx.cartItems.forEach((item) => {
         if (props.id === item.id) {
           console.log("Ovaj proizvod je vec u korpi, povecaj amount");
-          const updIdArray = ctx.cartItems.findIndex((obj => obj.id ===item.id))
-          ctx.cartItems[updIdArray].amount+=1
-          inCart=true
-          
+          const updIdArray = ctx.cartItems.findIndex(
+            (obj) => obj.id === item.id
+          );
+          ctx.cartItems[updIdArray].amount += 1;
+          inCart = true;
         }
       });
 
@@ -32,20 +33,20 @@ const MealMenu = (props) => {
           id: props.id,
           name: props.name,
           price: props.price,
-          amount:amount
+          amount: amount,
         });
       }
     }
-    ctx.updateTotalPrice()
+    ctx.updateTotalPrice();
     console.log(ctx.cartItems);
-
   };
-
 
   return (
     <div className={classes.mealCard}>
       <div className={classes.mealRow}>
-        <h2>{props.name}</h2>
+        <div className={classes.headingWrapper}>
+          <h2>{props.name}</h2>
+        </div>
       </div>
       <div className={classes.mealRow}>
         <h3>{props.price} RSD</h3>
